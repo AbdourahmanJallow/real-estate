52723151113f1096da4d6233428a4784d3bf2f50
AbdourahmanJallow <abdourahman87jallow@gmail.com>
2025-03-21 18:12:51 +0000

    config

--- src/data-source.ts
+++ src/data-source.ts
@@ -17,7 +17,7 @@ dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: process.env.DB_HOST || 'localhost',
+  host: process.env.DB_HOST || 'localhost', // use localhost for development
   port: Number(process.env.DB_PORT) || 5432,
   username: process.env.DB_USERNAME || 'postgres',
   password: process.env.DB_PASSWORD || 'password',
